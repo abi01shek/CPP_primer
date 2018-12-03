4f0d30a0a4f0012919a1b385a3395ed8e8625843
Abishek Ramdas <ramdasa@student.ethz.ch>
2018-12-03 16:45:32 +0100

    sales data header added

--- chapter_7_classes/sales_data_program.cpp
+++ chapter_7_classes/sales_data_program.cpp
@@ -1,61 +1,32 @@
+#include "Sales_data.h"
 #include<iostream>
-#include<vector>
-#include<string>
 
 using std::cin;
 using std::cout;
 using std::endl;
-using std::vector;
 using std::string;
 
-struct Sales_data{
-  //new members:operations on sales_data object
-  string isbn() const {return this->bookNo;}
-  Sales_data& combine(const Sales_data&);
-  double avg_price() const;
-  //data members
-  string bookNo;
-  unsigned units_sold = 0;
-  double revenue = 0.0;
-};
-
-//non member sales_data interface function
-Sales_data add(const Sales_data&, const Sales_data&);
-std::ostream &print(std::ostream&, const Sales_data&);
-std::istream &read(std::istream&, const Sales_data&);
-
-
-//scope operator 
-double Sales_data::avg_price() const{
-  if(units_sold)
-    return revenue/units_sold;
-  else
-    return 0;
-}
-
-Sales_data& Sales_data::combine(const Sales_data &rhs){
-  units_sold += rhs.units_sold;
-  revenue += rhs.revenue;
-  return *this;
-}
-
-int main() {
-  Sales_data total;
-  if(cin >> total.bookNo >> total.units_sold >> total.revenue){
-    Sales_data trans;
-    while(cin >> trans.bookNo >> trans.units_sold >> trans.revenue){
-      if(total.bookNo == trans.bookNo){
-	total.combine(trans);
-      }
-      else{
-	cout << total.bookNo << " " << total.units_sold << " " << total.revenue << endl;
-      }
+int main()
+{
+    Sales_data total;
+    if (cin >> total.bookNo >> total.units_sold >> total.revenue)
+    {
+        Sales_data trans;
+        while (cin >> trans.bookNo >> trans.units_sold >> trans.revenue) {
+            if (total.isbn() == trans.isbn())
+                total.combine(trans);
+            else {
+                cout << total.bookNo << " " << total.units_sold << " " << total.revenue << endl;
+                total = trans;
+            }
+        }
+        cout << total.bookNo << " " << total.units_sold << " " << total.revenue << endl;
     }
-    cout << total.bookNo << " " << total.units_sold << " " << total.revenue << endl;
-  }
-  else{
-    std::cerr << "No data?!" << endl;
-    return -1;
-  }
-  return 0;
+    else
+    {
+        std::cerr << "No data?!" << std::endl;
+        return -1;
+    }
+    
+    return 0;
 }
